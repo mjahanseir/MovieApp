4ee3a3c394d5da06d4f4e25a1b40ae56886f7ad2
mjahanseir <mjahanseir@gmail.com>
2022-04-11 18:39:22 -0600

    update vidoe system

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import Counter from "./components/counter";
+ReactDOM.render(<Counter />, document.getElementById("root"));
 
 reportWebVitals();
