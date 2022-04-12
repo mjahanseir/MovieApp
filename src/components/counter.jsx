d7bd1aeee498c41a36399fe8a2c55137a872fe77
mjahanseir <mjahanseir@gmail.com>
2022-04-11 23:05:51 -0600

    video commit

--- src/components/counter.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-class Counter extends Component {
-    state = {}
-    render() {
-        return <h1>Hello</h1>
-    }
-}
-
-export default Counter;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
-import Counter from "./components/counter";
-ReactDOM.render(<Counter />, document.getElementById("root"));
 
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
